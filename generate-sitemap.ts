4c7e70ec92ccfe2f6976461127344995de843f99
Aman377 <aman@excitesys.com>
2025-09-29 19:15:33 +0530

    deploy

--- generate-sitemap.ts
+++ generate-sitemap.ts
@@ -6,7 +6,7 @@ import routes from "./src/routesConfig.ts";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const baseUrl = "https://justdivescuba.com";
+const baseUrl = "https://justdivescuba.com"; 
 
 async function generateSitemap(urls: string[]) {
   try {
