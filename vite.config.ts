9967fdecfa240f8817bab4791dd09e5f2da955bb
Aman377 <aman@excitesys.com>
2025-09-29 19:12:30 +0530

    first deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
