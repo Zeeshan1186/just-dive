20f7e89293d41826645a53686678dd4ce2c9509b
Aman377 <aman@excitesys.com>
2025-10-06 13:34:06 +0530

    our team to our managment

--- src/components/WhyUs.tsx
+++ src/components/WhyUs.tsx
@@ -1,15 +1,16 @@
 import waves from "../assets/images/Bluewave.webp";
 import whyus from "../assets/images/whyus.png";
 import AJAY from "../assets/images/team/decosta.png";
-import SUSHIl from "../assets/images/team/managing-partner.png";
+// import SUSHIl from "../assets/images/team/managing-partner.png";
 import ARVIND from "../assets/images/team/ARVIND-BHADRAN.png";
-import SAINATH from "../assets/images/team/SAINATH.png";
-import HARIKRISHNAN from "../assets/images/team/HARIKRISHNAN.png";
-import VINOD from "../assets/images/team/VINOD-KUMAR.png";
-import BIMAL from "../assets/images/team/BIMAL-BIJU.png";
-import DHANUSH from "../assets/images/team/DHANUSH--B.png";
-import MUHAMMAD from "../assets/images/team/MUHAMMAD-FIROZ-KHAN.png";
-import ABHISHEK from "../assets/images/team/ABHISHEK-JAYAMON.png";
+import director from "../assets/images/team/director.webp";
+// import SAINATH from "../assets/images/team/SAINATH.png";
+// import HARIKRISHNAN from "../assets/images/team/HARIKRISHNAN.png";
+// import VINOD from "../assets/images/team/VINOD-KUMAR.png";
+// import BIMAL from "../assets/images/team/BIMAL-BIJU.png";
+// import DHANUSH from "../assets/images/team/DHANUSH--B.png";
+// import MUHAMMAD from "../assets/images/team/MUHAMMAD-FIROZ-KHAN.png";
+// import ABHISHEK from "../assets/images/team/ABHISHEK-JAYAMON.png";
 import Lottie from 'react-lottie-player';
 import experience from '../assets/Animation - 1750410355214.json';
 import result from '../assets/Animation - 1750410365589.json';
@@ -92,17 +93,17 @@ export default function WhyUs() {
                 <div className="flex flex-col justify-center items-center pb-6 px-4">
                     <img src={waves} alt="waves" className="mx-auto mb-2 w-8" />
                     <h2 className="text-3xl sm:text-4xl Trirong font-normal text-gray-800 mb-4">
-                        Meet Our Team
+                        Management Team
                     </h2>
                 </div>
 
                 {/* Team Grid */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-5 pb-10 px-4 sm:px-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 pb-10 px-4 sm:px-8">
 
                     {/* Team Member 0 */}
                     <div
                         className="relative flex flex-col justify-end items-start text-white p-4 
-               bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
+               bg-cover bg-center sm:w-[80%] w-full h-[60vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl"
                         style={{ backgroundImage: `url(${AJAY})` }}
@@ -115,7 +116,7 @@ export default function WhyUs() {
                     </div>
 
                     {/* Team Member 1 */}
-                    <div className="relative flex flex-col justify-end items-start text-white p-4 
+                    {/* <div className="relative flex flex-col justify-end items-start text-white p-4 
                bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${SUSHIl})` }}>
@@ -124,22 +125,34 @@ export default function WhyUs() {
                             <h3 className="text-lg font-semibold">Sushil Kumar Pillai</h3>
                             <p className="text-sm">Managing partner</p>
                         </div>
-                    </div>
+                    </div> */}
 
                     {/* Team Member 2 */}
                     <div className="relative flex flex-col justify-end items-start text-white p-4 
-               bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
+               bg-cover bg-center sm:w-[80%] w-full h-[60vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${ARVIND})` }}>
                         <div className="absolute inset-0 bg-black/20"></div>
                         <div className="relative z-10">
                             <h3 className="text-lg font-semibold">Arvind Bhadran</h3>
-                            <p className="text-sm">Finance manager</p>
+                            <p className="text-sm">Finance Manager</p>
+                        </div>
+                    </div>
+
+                    {/* Team Member 2 */}
+                    <div className="relative flex flex-col justify-end items-start text-white p-4 
+               bg-cover bg-center sm:w-[80%] w-full h-[60vh] rounded-lg overflow-hidden
+               transform transition-all duration-500 ease-in-out
+               hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${director})` }}>
+                        <div className="absolute inset-0 bg-black/20"></div>
+                        <div className="relative z-10">
+                            <h3 className="text-lg font-semibold">Mr. Girish Pillai</h3>
+                            <p className="text-sm">Director</p>
                         </div>
                     </div>
 
                     {/* Team Member 3 */}
-                    <div className="relative flex flex-col justify-end items-start text-white p-4 
+                    {/* <div className="relative flex flex-col justify-end items-start text-white p-4 
                bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${SAINATH})` }}>
@@ -148,10 +161,10 @@ export default function WhyUs() {
                             <h3 className="text-lg font-semibold">Sainath S</h3>
                             <p className="text-sm">Dive operation manager</p>
                         </div>
-                    </div>
+                    </div> */}
 
                     {/* Team Member 4 */}
-                    <div className="relative flex flex-col justify-end items-start text-white p-4 
+                    {/* <div className="relative flex flex-col justify-end items-start text-white p-4 
                bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${HARIKRISHNAN})` }}>
@@ -160,10 +173,10 @@ export default function WhyUs() {
                             <h3 className="text-lg font-semibold">Harikrishnan</h3>
                             <p className="text-sm">PADI dive instructor</p>
                         </div>
-                    </div>
+                    </div> */}
 
                     {/* Team Member 5 */}
-                    <div className="relative flex flex-col justify-end items-start text-white p-4 
+                    {/* <div className="relative flex flex-col justify-end items-start text-white p-4 
                bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${VINOD})` }}>
@@ -172,10 +185,10 @@ export default function WhyUs() {
                             <h3 className="text-lg font-semibold">Vinod Kumar D</h3>
                             <p className="text-sm">PADI dive instructor</p>
                         </div>
-                    </div>
+                    </div> */}
 
                     {/* Team Member 6 */}
-                    <div className="relative flex flex-col justify-end items-start text-white p-4 
+                    {/* <div className="relative flex flex-col justify-end items-start text-white p-4 
                bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${BIMAL})` }}>
@@ -184,10 +197,10 @@ export default function WhyUs() {
                             <h3 className="text-lg font-semibold">Bimal Biju</h3>
                             <p className="text-sm">PADI dive master</p>
                         </div>
-                    </div>
+                    </div> */}
 
                     {/* Team Member 7 */}
-                    <div className="relative flex flex-col justify-end items-start text-white p-4 
+                    {/* <div className="relative flex flex-col justify-end items-start text-white p-4 
                bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${DHANUSH})` }}>
@@ -196,10 +209,10 @@ export default function WhyUs() {
                             <h3 className="text-lg font-semibold">Dhanush B</h3>
                             <p className="text-sm">PADI dive master</p>
                         </div>
-                    </div>
+                    </div> */}
 
                     {/* Team Member 8 */}
-                    <div className="relative flex flex-col justify-end items-start text-white p-4 
+                    {/* <div className="relative flex flex-col justify-end items-start text-white p-4 
                bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${MUHAMMAD})` }}>
@@ -208,10 +221,10 @@ export default function WhyUs() {
                             <h3 className="text-lg font-semibold">Muhammad Firoz Khan</h3>
                             <p className="text-sm">PADI dive master</p>
                         </div>
-                    </div>
+                    </div> */}
 
                     {/* Team Member 9 */}
-                    <div className="relative flex flex-col justify-end items-start text-white p-4 
+                    {/* <div className="relative flex flex-col justify-end items-start text-white p-4 
                bg-cover bg-center w-full h-[50vh] rounded-lg overflow-hidden
                transform transition-all duration-500 ease-in-out
                hover:scale-105 hover:shadow-2xl" style={{ backgroundImage: `url(${ABHISHEK})` }}>
@@ -220,7 +233,7 @@ export default function WhyUs() {
                             <h3 className="text-lg font-semibold">Abhishek Jayamon</h3>
                             <p className="text-sm">PADI dive master</p>
                         </div>
-                    </div>
+                    </div> */}
 
                 </div>
 
