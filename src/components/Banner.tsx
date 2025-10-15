00c59700192971f514d6b841305dcb0472738df4
Aman377 <aman@excitesys.com>
2025-10-15 16:49:06 +0530

    buil err fix

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,7 +11,7 @@ import { format, parse } from "date-fns";
 import { CalendarIcon, ChevronDown } from "lucide-react";
 import { cn } from "@/lib/utils";
 import peoples from "../assets/images/Peoples.webp";
-import waves from "../assets/images/Waves.png";
+import waves from "../assets/images/Waves.webp";
 import { motion, AnimatePresence } from "framer-motion";
 import { useNavigate, useParams } from "react-router-dom";
 import { getactivePackages, getactivePackagesByLocation } from "@/services/apiService";
